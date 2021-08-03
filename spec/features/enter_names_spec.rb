89ac010b0cb9d598d702b67eac838f6ab720e4b2
Michael PS <michael.plasomscott@hotmail.co.uk>
2021-08-03 16:53:01 +0100

    Saves and Opens Page Displaying Names

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -4,6 +4,7 @@ feature 'Enter names' do
     fill_in :player_1_name, with: 'Michael'
     fill_in :player_2_name, with: 'Wesley'
     click_button 'Submit'
+    save_and_open_page
     expect(page).to have_content 'Michael vs. Wesley'
   end
 end
